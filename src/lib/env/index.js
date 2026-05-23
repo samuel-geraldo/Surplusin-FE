6dc29d310ca46cfe38e3cfce7951e3be91cd2a47
Adla Fayyaz <adlafayyaz@gmail.com>
2026-05-23 23:53:21 +0700

    fix: resolve conflicts and switch to MapLibre open source map

--- src/lib/env/index.js
+++ src/lib/env/index.js
@@ -5,6 +5,5 @@ export const env = {
   APP_NAME: viteEnv.VITE_APP_NAME || 'SurplusIn',
   MAP_TILE_URLS: viteEnv.VITE_MAP_TILE_URLS || '',
   GEOCODING_SEARCH_URL: viteEnv.VITE_GEOCODING_SEARCH_URL || '',
-  GOOGLE_MAPS_API_KEY: viteEnv.VITE_GOOGLE_MAPS_API_KEY || '',
-  USE_MOCK_API: viteEnv.VITE_USE_MOCK_API !== 'false',
+  GOOGLE_MAPS_API_KEY: viteEnv.VITE_GOOGLE_MAPS_API_KEY || 'AIzaSyAxvkMdHwDpYBUi62RVVoO4O9SmG_AgPp0',
 };
